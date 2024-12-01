09290506dff24c0432ab306b6ffb168cb1146a86
Ahmad Nur Fauzan <anfauzan010104@gmail.com>
2024-12-01 13:20:24 +0800

    desain welcome login regis

--- MD/Code/Nutrisee/app/src/main/java/com/bangkit/nutrisee/ui/signup/SignupActivity.kt
+++ MD/Code/Nutrisee/app/src/main/java/com/bangkit/nutrisee/ui/signup/SignupActivity.kt
@@ -24,11 +24,7 @@ class SignupActivity : AppCompatActivity() {
         setContentView(R.layout.activity_signup)
 
         // Set up window insets for edge-to-edge display
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
 
         val usernameInput = findViewById<EditText>(R.id.usernameInput)
         val emailInput = findViewById<EditText>(R.id.emailInput)
