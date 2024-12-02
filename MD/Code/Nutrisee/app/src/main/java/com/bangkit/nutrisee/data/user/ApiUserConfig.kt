0fdf5a2fe469fea3d6b797469d5cde8d96b775b0
Ahmad Nur Fauzan <anfauzan010104@gmail.com>
2024-12-03 03:40:38 +0800

    perbaikan error, problem di tampilan nav bottomnya

--- MD/Code/Nutrisee/app/src/main/java/com/bangkit/nutrisee/data/user/ApiUserConfig.kt
+++ MD/Code/Nutrisee/app/src/main/java/com/bangkit/nutrisee/data/user/ApiUserConfig.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiUserConfig {
     companion object {
-        private const val BASE_URL = "http://192.168.202.100:5000/api/v1/" // Ganti dengan URL API Anda
+        private const val BASE_URL = "http://122.248.242.244:3000/api/v1/" // Ganti dengan URL API Anda
 
         fun getApiService(): ApiUserService {
             // Interceptor untuk logging request dan response
