c98bb6821f0f2729dd23c8bf4bf6c44359c57802
ahmadhidayat22 <ahmadnurhidayat271@gmail.com>
2024-12-07 19:03:02 +0800

    auth google android, reset pass, manage product, news

--- CC/routes/index.js
+++ CC/routes/index.js
@@ -10,24 +10,40 @@ import {
     logout,
     getResetPassword,
     profile,
+    verifyGoogleLogin
 
 } from '../controller/UserController.js';
+
+import { 
+    getAllProducts, 
+    getProductById, 
+    createProduct
+} from '../controller/ProductController.js';
 
 import { verifyToken } from '../middleware/VerifyToken.js';
 import { refreshToken } from '../controller/RefreshToken.js';
 import { fetchNews } from '../controller/NewsController.js';
 import { hardLimiter } from '../middleware/limiter.js';
+
 const router = express.Router();
 
-
+//news
 router.get('/news', verifyToken, fetchNews);
+
+//produk
+router.get('/product', verifyToken, getAllProducts);
+router.get('/product/:id', verifyToken, getProductById);
+router.post('/product', verifyToken, createProduct);
+
+
 router.get('/users',verifyToken, getUser);
-router.get('/profile', verifyToken, profile)
+router.get('/profile',verifyToken, profile)
 router.post('/register', Register);
 router.post('/login', Login);
-router.get('/token', refreshToken);
-router.get('/auth/google', redirectOauthLogin);
-router.get('/auth/google/callback', callbackOauthLogin);
+router.post('/token', refreshToken);
+
+// router.get('/auth/google', redirectOauthLogin);
+// router.get('/auth/google/callback', callbackOauthLogin);
 
 router.post('/forgot-password', hardLimiter, forgotPassword);
 router.get('/reset-password/:id/:token', getResetPassword);
@@ -35,17 +51,6 @@ router.post('/reset-password/:id/:token', hardLimiter, resetPassword);
 
 router.delete('/logout', logout);
 
-
-
-// router.get('/test-delay', (req,res) => {
-//     const timeout = [50,100, 200,300];
-//     const rand = Math.floor(Math.random() * timeout.length);
-//     const randomTimeout = timeout[rand];
-//     setTimeout(() => {
-//         res.send("testing timeout");
-//     }, randomTimeout)
-// })
-
-
+router.get('/verify-google-login', verifyGoogleLogin);
 
 export default router;
